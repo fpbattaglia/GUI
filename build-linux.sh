ec0d8fe7d2c9890a24b97795a098e72cfd601b3a
A student of Francesco <F.Battaglia@science.ru.nl>
2015-08-14 19:15:18 +0200

    further simplified build process

--- build-linux.sh
+++ build-linux.sh
@@ -3,24 +3,12 @@
 
 export SOURCE_DIR=`pwd`
 export PYTHON_LIBS=$SOURCE_DIR/python-libs
-export CONDA_HOME=/home/fpbatta/anaconda
+export CONDA_HOME=`which python | sed "s/\/bin\/python//"`
+echo $CONDA_HOME
 
 mkdir $PYTHON_LIBS
 ln -sf $CONDA_HOME/lib/libpython2.7.so $PYTHON_LIBS/libpython2.7.so
 ln -sf $CONDA_HOME/lib/libpython2.7.so.1.0 $PYTHON_LIBS/libpython2.7.so.1.0
-
-
-
-cat <<FINE >run_open_ephys
-#!/bin/bash
-
-export PYTHONHOME=${CONDA_HOME}
-`pwd`/Builds/Linux/build/open-ephys
-
-FINE
-
-
-chmod +x run_open_ephys
 
 cd Builds/Linux/
 make 
