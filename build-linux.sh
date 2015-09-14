00f91b2da782647dd83eee97ee9937d8db854400
A student of Francesco <F.Battaglia@science.ru.nl>
2015-09-14 16:20:16 +0200

    got forkserver to work under linux (setting executable to python)

--- build-linux.sh
+++ build-linux.sh
@@ -6,9 +6,10 @@ export PYTHON_LIBS=$SOURCE_DIR/python-libs
 export CONDA_HOME=`which python | sed "s/\/bin\/python//"`
 echo $CONDA_HOME
 
+rm -rf $PYTHON_LIBS
 mkdir $PYTHON_LIBS
-ln -sf $CONDA_HOME/lib/libpython2.7.so $PYTHON_LIBS/libpython2.7.so
-ln -sf $CONDA_HOME/lib/libpython2.7.so.1.0 $PYTHON_LIBS/libpython2.7.so.1.0
+ln -sf $CONDA_HOME/lib/libpython3.4m.so $PYTHON_LIBS/libpython3.4m.so
+ln -sf $CONDA_HOME/lib/libpython3.4m.so.1.0 $PYTHON_LIBS/libpython3.4m.so.1.0
 
 cd Builds/Linux/
 make 
